b85209f3e6e84077c7acbe1040967cea6b54ee5f
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-11-30 08:37:17 +0000

    Conversation mybatis와 DAO 추가

--- Kostagram/src/com/kostagram/service/dao/ConversationDAO.java
+++ Kostagram/src/com/kostagram/service/dao/ConversationDAO.java
@@ -1,5 +1,13 @@
 package com.kostagram.service.dao;
 
-public interface ConversationDAO {
+import java.util.List;
 
+import com.kostagram.service.beans.ConversationVO;
+import com.kostagram.service.beans.UserInfoVO;
+
+public interface ConversationDAO {
+    public boolean insert(ConversationVO newConversation);
+    public boolean delete(ConversationVO deleteConversation);
+    public boolean update(ConversationVO updateConversation);
+    public List<ConversationVO> selectList(UserInfoVO user);
 }
--- Kostagram/src/com/kostagram/service/dao/ConversationDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/ConversationDAOImpl.java
@@ -1,5 +1,42 @@
 package com.kostagram.service.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kostagram.service.beans.ConversationVO;
+import com.kostagram.service.beans.UserInfoVO;
+
 public class ConversationDAOImpl implements ConversationDAO {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
+    public boolean insert(ConversationVO newConversation) {
+	int result = sqlSession.insert("conversation.insert", newConversation);
+	if ( result == 1 )
+	    return true;
+	return false;
+    }
+
+    @Override
+    public boolean delete(ConversationVO deleteConversation) {
+	int result = sqlSession.delete("conversation.delete", deleteConversation);
+	return false;
+    }
+
+    @Override
+    public boolean update(ConversationVO updateConversation) {
+	int result = sqlSession.update("conversation.update", updateConversation);
+	return false;
+    }
+
+    @Override
+    public List<ConversationVO> selectList(UserInfoVO user) {
+	List<ConversationVO> convList = sqlSession.selectList("conversation.selectList", user);
+	return convList;
+    }
 
 }
