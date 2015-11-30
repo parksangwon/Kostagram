7e99e54503ca04e134d6bdec936bdd082e5b0901
hagutwo1991 <hagutwo1991@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-11-30 07:41:07 +0000

    follow dao,impl,.xml 업로드

--- Kostagram/src/com/kostagram/service/dao/FollowDAO.java
+++ Kostagram/src/com/kostagram/service/dao/FollowDAO.java
@@ -1,6 +1,10 @@
 package com.kostagram.service.dao;
 
+import com.kostagram.service.beans.FollowVO;
+
 public interface FollowDAO {
-	
+	public boolean insert(FollowVO follow);
+    public boolean delete(FollowVO follow); //프로필수정
+    
 
 }
--- Kostagram/src/com/kostagram/service/dao/FollowDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/FollowDAOImpl.java
@@ -1,5 +1,26 @@
 package com.kostagram.service.dao;
 
-public class FollowDAOImpl implements FollowDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.kostagram.service.beans.FollowVO;
+
+public class FollowDAOImpl implements FollowDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public boolean insert(FollowVO follow) {
+		int insert= sqlSession.insert("follow.insert",follow);
+		if(insert==1)return true;
+		
+		return false;
+	}
+	
+	@Override
+	public boolean delete(FollowVO follow) {
+		return false;
+	}
+	
+	
 }
