245fcba2ed5bc0868b42742aa14fa24b667f6967
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-01 02:35:38 +0000

    기능 정의

--- Kostagram/src/com/kostagram/service/dao/ConversationReplyDAO.java
+++ Kostagram/src/com/kostagram/service/dao/ConversationReplyDAO.java
@@ -1,5 +1,12 @@
 package com.kostagram.service.dao;
 
-public interface ConversationReplyDAO {
+import java.util.List;
 
+import com.kostagram.service.beans.ConversationReplyVO;
+import com.kostagram.service.beans.ConversationVO;
+
+public interface ConversationReplyDAO {
+    public Boolean insert (ConversationReplyVO newConversationReply);
+    public Boolean delete (ConversationReplyVO delConversationReply);
+    public List<ConversationReplyVO> findAll (ConversationVO conversation);
 }
--- Kostagram/src/com/kostagram/service/dao/ConversationReplyDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/ConversationReplyDAOImpl.java
@@ -1,5 +1,38 @@
 package com.kostagram.service.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kostagram.service.beans.ConversationReplyVO;
+import com.kostagram.service.beans.ConversationVO;
+
 public class ConversationReplyDAOImpl implements ConversationReplyDAO{
+
+    @Autowired
+    private SqlSession sqlSession;
+    
+    @Override
+    public Boolean insert(ConversationReplyVO newConversationReply) {
+	int result = sqlSession.insert("conversationReply.insert", newConversationReply);
+	if ( result == 1 )
+	    return true;
+	return false;
+    }
+
+    @Override
+    public Boolean delete(ConversationReplyVO delConversationReply) {
+	int result = sqlSession.delete("conversationReply.delete", delConversationReply);
+	if ( result == 1 )
+	    return true;
+	return null;
+    }
+
+    @Override
+    public List<ConversationReplyVO> findAll(ConversationVO conversation) {
+	List<ConversationReplyVO> replyList = sqlSession.selectList("conversationReply.findAll", conversation);
+	return replyList;
+    }
 
 }
