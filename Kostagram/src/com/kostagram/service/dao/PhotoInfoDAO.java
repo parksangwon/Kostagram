750140b2ccd4e3f7965f5ec69febea17799481b8
hagutwo1991 <hagutwo1991@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-01 08:01:05 +0000

    photoInfoDAO, Impl, .xml 수정~

--- Kostagram/src/com/kostagram/service/dao/PhotoInfoDAO.java
+++ Kostagram/src/com/kostagram/service/dao/PhotoInfoDAO.java
@@ -1,5 +1,12 @@
 package com.kostagram.service.dao;
 
-public interface PhotoInfoDAO {
+import java.util.List;
 
+import com.kostagram.service.beans.PhotoInfoVO;
+
+public interface PhotoInfoDAO {
+	public boolean insert(PhotoInfoVO photo);
+	public boolean update(PhotoInfoVO photo);
+	public boolean delete(PhotoInfoVO photo);
+	public List<PhotoInfoVO> getTimeline();
 }
--- Kostagram/src/com/kostagram/service/dao/PhotoInfoDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/PhotoInfoDAOImpl.java
@@ -1,5 +1,37 @@
 package com.kostagram.service.dao;
 
-public class PhotoInfoDAOImpl implements PhotoInfoDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kostagram.service.beans.PhotoInfoVO;
+import com.kostagram.service.beans.UserInfoVO;
+
+public class PhotoInfoDAOImpl implements PhotoInfoDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public boolean insert(PhotoInfoVO photo) {
+		int insert= sqlSession.insert("photoInfo.insert",photo);
+		if(insert==1)return true;
+		
+		return false;
+	}
+	@Override
+	public boolean delete(PhotoInfoVO photo) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(PhotoInfoVO photo) {
+		return false;
+	}
+	
+
+	@Override
+	public List<PhotoInfoVO> getTimeline() {
+		return sqlSession.selectList("photoInfo.getTimeline");
+	}
 }
