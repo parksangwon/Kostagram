0574bcbb64e456f4f4f43182402c17e17e560229
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-01 03:06:09 +0000

    Hashtag query, dao 작성

--- Kostagram/src/com/kostagram/service/dao/HashtagDAO.java
+++ Kostagram/src/com/kostagram/service/dao/HashtagDAO.java
@@ -1,5 +1,11 @@
 package com.kostagram.service.dao;
 
-public interface HashtagDAO {
+import java.util.List;
 
+import com.kostagram.service.beans.HashtagVO;
+
+public interface HashtagDAO {
+    public Boolean insert(HashtagVO hashtag);
+    public Boolean update(HashtagVO hashtag);
+    public List<HashtagVO> findHashtagList(HashtagVO token);
 }
--- Kostagram/src/com/kostagram/service/dao/HashtagDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/HashtagDAOImpl.java
@@ -1,5 +1,37 @@
 package com.kostagram.service.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kostagram.service.beans.HashtagVO;
+
 public class HashtagDAOImpl implements HashtagDAO {
+
+    @Autowired
+    private SqlSession sqlSession;
+    
+    @Override
+    public Boolean insert(HashtagVO hashtag) {
+	int result = sqlSession.insert("hashtag.insert", hashtag);
+	if ( result == 1 )
+	    return true;
+	return null;
+    }
+
+    @Override
+    public Boolean update(HashtagVO hashtag) {
+	int result = sqlSession.update("hashtag.update", hashtag);
+	if ( result == 1 )
+	    return true;
+	return null;
+    }
+
+    @Override
+    public List<HashtagVO> findHashtagList(HashtagVO token) {
+	List<HashtagVO> hashtagList = sqlSession.selectList("hashtag.findAllByToken", token);
+	return hashtagList;
+    }
 
 }
