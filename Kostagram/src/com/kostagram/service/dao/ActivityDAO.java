1a34a23d86c0c9134da2e598183431990ae2dfe0
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-02 06:45:00 +0000

    selectList 추가

--- Kostagram/src/com/kostagram/service/dao/ActivityDAO.java
+++ Kostagram/src/com/kostagram/service/dao/ActivityDAO.java
@@ -1,7 +1,11 @@
 package com.kostagram.service.dao;
 
+import java.util.List;
+
 import com.kostagram.service.beans.ActivityVO;
+import com.kostagram.service.beans.UserInfoVO;
 
 public interface ActivityDAO {
 	public boolean insert(ActivityVO activity);
+	public List<ActivityVO> activitiyList(UserInfoVO user);
 }
--- Kostagram/src/com/kostagram/service/dao/ActivityDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/ActivityDAOImpl.java
@@ -1,20 +1,30 @@
 package com.kostagram.service.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kostagram.service.beans.ActivityVO;
+import com.kostagram.service.beans.UserInfoVO;
 
 public class ActivityDAOImpl implements ActivityDAO {
-	
-	@Autowired
-	private SqlSession sqlSession;
-	
-	public boolean insert(ActivityVO activity) {
-		
-		int result = sqlSession.insert("activity.insert", activity);
-		if ( result == 1 ) 
-			return true;
-		return false;
-	}
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    public boolean insert(ActivityVO activity) {
+
+	int result = sqlSession.insert("activity.insert", activity);
+	if (result == 1)
+	    return true;
+	return false;
+    }
+
+    @Override
+    public List<ActivityVO> activitiyList(UserInfoVO user) {
+	List<ActivityVO> activityList = sqlSession.selectList("activity.selectList", user);
+	return activityList;
+    }
+
 }
