d8b3d0206daf62afe1683dd583bf477471bc2490
tam0605 <tam0605@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-07 00:50:12 +0000

    ArticleVO 추가

---
+++ Kostagram/src/com/kostagram/service/beans/ArticleVO.java
@@ -0,0 +1,20 @@
+package com.kostagram.service.beans;
+
+import java.util.Date;
+import java.util.List;
+
+public class ArticleVO {
+	private PhotoInfoVO photo;
+	private List<LikeVO> like;
+	private List<CommentVO> comment;
+	
+	
+	public ArticleVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	
+	
+}
