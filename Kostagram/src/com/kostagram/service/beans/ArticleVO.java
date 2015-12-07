7ec56a8b9d53d701444675a0e1fbad902a8ea45f
tam0605 <tam0605@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-07 00:51:09 +0000

    ArticleVO get,set 추가

--- Kostagram/src/com/kostagram/service/beans/ArticleVO.java
+++ Kostagram/src/com/kostagram/service/beans/ArticleVO.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 public class ArticleVO {
+	
 	private PhotoInfoVO photo;
 	private List<LikeVO> like;
 	private List<CommentVO> comment;
@@ -11,6 +12,30 @@ public class ArticleVO {
 	
 	public ArticleVO() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public List<LikeVO> getLike() {
+		return like;
+	}
+
+
+	public void setLike(List<LikeVO> like) {
+		this.like = like;
+	}
+
+
+	public List<CommentVO> getComment() {
+		return comment;
+	}
+
+
+	public void setComment(List<CommentVO> comment) {
+		this.comment = comment;
+	}
+
+
+	public void setPhoto(PhotoInfoVO photo) {
+		this.photo = photo;
 	}
 
 
