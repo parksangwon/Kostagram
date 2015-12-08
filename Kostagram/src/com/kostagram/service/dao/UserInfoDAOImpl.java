3b626bda42cb5d4d68247b7e458e7e6c4587b158
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-08 05:39:22 +0000

    smtp 기능 추가

--- Kostagram/src/com/kostagram/service/dao/UserInfoDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/UserInfoDAOImpl.java
@@ -8,48 +8,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.kostagram.service.beans.UserInfoVO;
 
 public class UserInfoDAOImpl implements UserInfoDAO {
-	
-	@Autowired
-	private SqlSession sqlSession;
-	
-	@Override
-	public boolean insert(UserInfoVO user) {
-		System.out.println("DAO sysout : " + user);
-		int insert= sqlSession.insert("userInfo.insert",user);
-		if(insert==1)return true;
-		
-		return false;
-	}
-	
-	@Override
-	public boolean update(UserInfoVO user) {
-		return false;
-	}
-	
-	@Override
-	public UserInfoVO findEmail(UserInfoVO user) {
-		return sqlSession.selectOne("userInfo.findEmail", user);
-	}
-	
-	@Override
-	public UserInfoVO findNickname(UserInfoVO user) {
-	    	System.out.println("UserDAO : " + user);
-		return sqlSession.selectOne("userInfo.findNickname", user);
-	}
-	
-	@Override
-	public List<UserInfoVO> findList(String token) {
-		return sqlSession.selectList("userInfo.findList",token);
-	}
-	
-	@Override
-	public boolean pwCheck(UserInfoVO user){
-		return sqlSession.selectOne("userInfo.pwCheck", user);
-	}
 
-	@Override
-	public boolean pwUpdate(UserInfoVO user) {
-		return false;
-	}
-	
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
+    public boolean insert(UserInfoVO user) {
+	System.out.println("DAO sysout : " + user);
+	int insert = sqlSession.insert("userInfo.insert", user);
+	if (insert == 1)
+	    return true;
+
+	return false;
+    }
+
+    @Override
+    public boolean update(UserInfoVO user) {
+	return false;
+    }
+
+    @Override
+    public UserInfoVO findEmail(UserInfoVO user) {
+	return sqlSession.selectOne("userInfo.findEmail", user);
+    }
+
+    @Override
+    public UserInfoVO findNickname(UserInfoVO user) {
+	System.out.println("UserDAO : " + user);
+	return sqlSession.selectOne("userInfo.findNickname", user);
+    }
+
+    @Override
+    public UserInfoVO findUser(String token) {
+	UserInfoVO user = sqlSession.selectOne("userInfo.findUser", token);
+	return user;
+    }
+
+    @Override
+    public List<UserInfoVO> findList(String token) {
+	return sqlSession.selectList("userInfo.findList", token);
+    }
+
+    @Override
+    public boolean pwCheck(UserInfoVO user) {
+	return sqlSession.selectOne("userInfo.pwCheck", user);
+    }
+
+    @Override
+    public boolean pwUpdate(UserInfoVO user) {
+	return false;
+    }
+
 }
