d70d6535597b159fe1f0ea90a5d79684e8a05acf
ourtree4 <ourtree4@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-10 01:33:02 +0000

    getPhoto 메서드 추가

--- Kostagram/src/com/kostagram/service/beans/ArticleVO.java
+++ Kostagram/src/com/kostagram/service/beans/ArticleVO.java
@@ -4,42 +4,36 @@ import java.util.Date;
 import java.util.List;
 
 public class ArticleVO {
-	
-	private PhotoInfoVO photo;
-	private List<LikeVO> like;
-	private List<CommentVO> comment;
-	
-	
-	public ArticleVO() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public List<LikeVO> getLike() {
-		return like;
-	}
 
+    private PhotoInfoVO photo;
+    private List<LikeVO> like;
+    private List<CommentVO> comment;
 
-	public void setLike(List<LikeVO> like) {
-		this.like = like;
-	}
+    public ArticleVO() {
+	// TODO Auto-generated constructor stub
+    }
 
+    public List<LikeVO> getLike() {
+	return like;
+    }
 
-	public List<CommentVO> getComment() {
-		return comment;
-	}
+    public void setLike(List<LikeVO> like) {
+	this.like = like;
+    }
 
+    public List<CommentVO> getComment() {
+	return comment;
+    }
 
-	public void setComment(List<CommentVO> comment) {
-		this.comment = comment;
-	}
+    public void setComment(List<CommentVO> comment) {
+	this.comment = comment;
+    }
 
+    public void setPhoto(PhotoInfoVO photo) {
+	this.photo = photo;
+    }
 
-	public void setPhoto(PhotoInfoVO photo) {
-		this.photo = photo;
-	}
-
-
-	
-	
-	
+    public PhotoInfoVO getPhoto() {
+	return photo;
+    }
 }
