351c3ff17433effd1692d787972d88e3b7c43eb6
tjdsh102 <tjdsh102@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-14 06:49:36 +0000

    findlist, delete 메소드 추가

--- Kostagram/src/com/kostagram/service/dao/ReportDAO.java
+++ Kostagram/src/com/kostagram/service/dao/ReportDAO.java
@@ -1,11 +1,12 @@
 package com.kostagram.service.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.kostagram.service.beans.ReportVO;
 
 public interface ReportDAO {
 	public boolean insert(ReportVO report);
-	public List<ReportVO> findList(ReportVO report);
-	
+	public List<HashMap> findList(ReportVO report);
+	public boolean delete(ReportVO report);
 }
