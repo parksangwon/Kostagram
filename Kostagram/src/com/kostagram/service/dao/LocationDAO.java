f36bc58ed22212bfcff2e678c74d182138e46b32
tam0605 <tam0605@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-15 06:06:11 +0000

    location.xml, LocationDao, LocationDAOImpl 내용 추가

--- Kostagram/src/com/kostagram/service/dao/LocationDAO.java
+++ Kostagram/src/com/kostagram/service/dao/LocationDAO.java
@@ -1,5 +1,15 @@
 package com.kostagram.service.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.kostagram.service.beans.LocationVO;
+import com.kostagram.service.beans.UserInfoVO;
+
 public interface LocationDAO {
+	
+	public boolean insert(LocationVO location);
+	public List<HashMap> findPhotoMap(UserInfoVO user);
+	
 
 }
