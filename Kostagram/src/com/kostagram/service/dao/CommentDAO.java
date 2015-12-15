a77a715b1544028ec8970632889470326e12fc8a
tjdsh102 <tjdsh102@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-15 11:11:27 +0000

    adsfafa

--- Kostagram/src/com/kostagram/service/dao/CommentDAO.java
+++ Kostagram/src/com/kostagram/service/dao/CommentDAO.java
@@ -1,5 +1,6 @@
 package com.kostagram.service.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.kostagram.service.beans.CommentVO;
@@ -7,5 +8,5 @@ import com.kostagram.service.beans.CommentVO;
 public interface CommentDAO {
 	public boolean insert(CommentVO comment);
 	public boolean delete(CommentVO comment);
-	public List<CommentVO> getCommentByPhotoId(CommentVO comment);
+	public List<HashMap> getCommentByPhotoId(String pid);
 }
