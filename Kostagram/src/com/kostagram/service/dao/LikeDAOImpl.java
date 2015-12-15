c41c08e95c54d5eee4256324366bc8a654291da2
hagutwo1991 <hagutwo1991@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-15 06:01:53 +0000

    like dao,impl 수정

--- Kostagram/src/com/kostagram/service/dao/LikeDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/LikeDAOImpl.java
@@ -3,6 +3,7 @@ package com.kostagram.service.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.kostagram.service.beans.FollowVO;
 import com.kostagram.service.beans.LikeVO;
 
 public class LikeDAOImpl implements LikeDAO {
@@ -15,6 +16,12 @@ public class LikeDAOImpl implements LikeDAO {
 		if(insert==1)return true;
 		
 		return false;
+	}
+	
+	@Override
+	public LikeVO check(LikeVO like)
+	{
+		return sqlSession.selectOne("like.check", like);
 	}
 	
 	@Override
