fade72f8728e558bc16c4db95561b962551732f4
tam0605 <tam0605@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-15 06:06:11 +0000

    location.xml, LocationDao, LocationDAOImpl 내용 추가

--- Kostagram/src/com/kostagram/service/dao/LocationDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/LocationDAOImpl.java
@@ -1,5 +1,29 @@
 package com.kostagram.service.dao;
 
-public class LocationDAOImpl implements LocationDAO {
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kostagram.service.beans.LocationVO;
+import com.kostagram.service.beans.UserInfoVO;
+
+public class LocationDAOImpl implements LocationDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public boolean insert(LocationVO location) {
+		int insert= sqlSession.insert("location.insert",location);
+		if(insert==1)return true;
+		
+		return false;
+	}
+	
+	@Override
+	public List<HashMap> findPhotoMap(UserInfoVO user) {
+		return sqlSession.selectList("location.findPhotoMap",user);
+	}
+	
 }
