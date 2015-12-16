3a7f349b2418034ef6716e39f5a98e5bf2129ec2
cho0kyung <cho0kyung@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-16 06:24:59 +0000

    hashtag 검색완료

--- Kostagram/src/com/kostagram/service/dao/HashtagDAO.java
+++ Kostagram/src/com/kostagram/service/dao/HashtagDAO.java
@@ -3,10 +3,12 @@ package com.kostagram.service.dao;
 import java.util.List;
 
 import com.kostagram.service.beans.HashtagVO;
+import com.kostagram.service.beans.PhotoInfoVO;
 
 public interface HashtagDAO {
     public Boolean insert(HashtagVO hashtag);
     public Boolean update(HashtagVO hashtag);
     public List<HashtagVO> findHashtagList(HashtagVO token);
     public List<HashtagVO> searchHashtag(HashtagVO hashtag);
+    public List<PhotoInfoVO> selectHashtag(String hashtag);
 }
