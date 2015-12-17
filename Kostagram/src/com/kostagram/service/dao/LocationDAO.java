5a7793e037dff151dfc5105031c8c2646cecbf61
cho0kyung <cho0kyung@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-17 03:10:51 +0000

    장소검색 기능 추가

--- Kostagram/src/com/kostagram/service/dao/LocationDAO.java
+++ Kostagram/src/com/kostagram/service/dao/LocationDAO.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.kostagram.service.beans.LocationVO;
+import com.kostagram.service.beans.PhotoInfoVO;
 import com.kostagram.service.beans.UserInfoVO;
 
 public interface LocationDAO {
 	
 	public boolean insert(LocationVO location);
 	public List<HashMap> findPhotoMap(UserInfoVO user);
-	
-
+	public List<HashMap> searchLocation(String location);
+	public List<PhotoInfoVO> selectPlace(String location);
+	public List<HashMap> selectLocationMap();
 }
