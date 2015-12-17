e07484e1dabc012321be3b96d28bca7bcd4f0342
cho0kyung <cho0kyung@21878b80-6d25-0510-8e30-deb5625ac93d>
2015-12-17 03:10:51 +0000

    장소검색 기능 추가

--- Kostagram/src/com/kostagram/service/dao/LocationDAOImpl.java
+++ Kostagram/src/com/kostagram/service/dao/LocationDAOImpl.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kostagram.service.beans.LocationVO;
+import com.kostagram.service.beans.PhotoInfoVO;
 import com.kostagram.service.beans.UserInfoVO;
 
 public class LocationDAOImpl implements LocationDAO {
@@ -15,7 +16,7 @@ public class LocationDAOImpl implements LocationDAO {
 	
 	@Override
 	public boolean insert(LocationVO location) {
-		int insert= sqlSession.insert("location.insert",location);
+		int insert= sqlSession.insert("location.insert", location);
 		if(insert==1)return true;
 		
 		return false;
@@ -23,7 +24,22 @@ public class LocationDAOImpl implements LocationDAO {
 	
 	@Override
 	public List<HashMap> findPhotoMap(UserInfoVO user) {
-		return sqlSession.selectList("location.findPhotoMap",user);
+		return sqlSession.selectList("location.findPhotoMap", user);
+	}
+	
+	@Override
+	public List<HashMap> searchLocation(String location) {
+		return sqlSession.selectList("location.searchLocation", location);
+	}
+	
+	@Override
+    public List<PhotoInfoVO> selectPlace(String location) {
+    	return sqlSession.selectList("location.selectLocation", location);
+    }
+	
+	@Override
+	public List<HashMap> selectLocationMap() {
+		return sqlSession.selectList("location.selectLocationMap");
 	}
 	
 }
